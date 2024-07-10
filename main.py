592b2651609d76987664320ed5dea7eef3ac7092
haibingxiong <haibingxiong9@gmail.com>
2024-07-10 23:52:15 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ with st.sidebar:
     key=st.text_input("请输入OpenAI API密钥",type='password')
     st.markdown("[如何获取OpenAI API密钥？](https://openai.com/index/chatgpt/)")
 #初始聊天界面：
-st.session_state["message"]=[{"role":"ai",
+if "memory" not in st.session_state:
+    st.session_state["message"]=[{"role":"ai",
                               "context":"你好，思颖机器人为您服务，有什么可以帮到你的吗？"}]
+print(st.session_state["message"])
 for message in st.session_state["message"]:
     st.chat_message(message["role"]).write(message["context"])
 ##询问开始
@@ -20,9 +22,10 @@ if prompt:
         st.stop()
     else:
         if "memory" not in st.session_state:
+            print("ok")
             model = ChatOpenAI(model='gpt-3.5-turbo', openai_api_key=key, openai_api_base="https://api.aigc369.com/v1")
             st.session_state["memory"] = ConversationSummaryBufferMemory(
-                llm=model, max_token_limit=2000, return_messages=True
+                llm=model, max_token_limit=5000, return_messages=True
             )
         st.chat_message('human').write(prompt)
         st.session_state["message"].append({"role":"human","context":prompt})
